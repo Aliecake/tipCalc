70f3c5df1b29db21b8a1d04b4f9d5431662347c1
Lauren Eggers <neyabenz@gmail.com>
2018-10-09 15:26:31 -0400

    labels added. Rounds to nearest penny.

--- tip.js
+++ tip.js
@@ -1,9 +1,14 @@
-
+/*to do: Varying tip amounts
+*styling
+*manifest, icons, theme
+*aria labels
+*different state taxes
+*/
 function calc() {
     const cost = Number(document.getElementById('billTotal').value);
     let meal = cost + cost * .625;
-    const total = cost + meal * .15;
-    const tipAmount = cost * .15;
+    const total = (cost + meal * .15).toFixed(2);
+    const tipAmount = (cost * .15).toFixed(2);
     document.getElementById('tip').innerHTML = `$${tipAmount}`;
     document.getElementById('total').innerHTML = `$${total}`;
 }
